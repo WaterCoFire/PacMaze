8656736ccd8e80a1b8b4e40d94116885626afc55
WaterCoFire <117800515+WaterCoFire@users.noreply.github.com>
2025-05-21 19:42:01 +0800

    No dead ends check logic added

--- Assets/Scripts/MapEditor/TileChecker.cs
+++ Assets/Scripts/MapEditor/TileChecker.cs
@@ -74,7 +74,7 @@ namespace MapEditor {
             int[,] distance = new int[11, 11]; // Record the shortest distance for each grid
 
             // Store tiles that are inaccessible or at a distance greater than 22
-            List<(int, int)> unreachableTiles = new List<(int, int)>();
+            List<(int, int)> invalidTiles = new List<(int, int)>();
 
             // Breadth-first search (BFS) queue
             // initialised by adding the centre (5, 5) to the queue
@@ -89,13 +89,36 @@ namespace MapEditor {
 
                 // If the current distance is greater than 22, it is marked as unreachable
                 if (distance[x, y] > 22) {
-                    unreachableTiles.Add((x, y));
+                    invalidTiles.Add((x, y));
                     continue;
                 }
 
                 // Get horizontal wall status and vertical wall status from wall data
                 bool[,] horizontalWallStatus = wallData.HorizontalWallStatus;
                 bool[,] verticalWallStatus = wallData.VerticalWallStatus;
+
+                // Check the num of its neighbours, if there's only one, it is a dead end tile
+                int neighboursCount = 0;
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + _dx[i];
+                    int ny = y + _dy[i];
+
+                    if (nx >= 0 && nx < 11 && ny >= 0 && ny < 11) {
+                        if (i == 0 && x > nx && !horizontalWallStatus[nx, y]) // Wall above
+                            neighboursCount++;
+                        if (i == 1 && x < nx && !horizontalWallStatus[x, y]) // Wall below
+                            neighboursCount++;
+                        if (i == 2 && y > ny && !verticalWallStatus[x, ny]) // Wall at the left
+                            neighboursCount++;
+                        if (i == 3 && y < ny && !verticalWallStatus[x, y]) // Wall at the right
+                            neighboursCount++;
+                    }
+                }
+
+                // If there is only one neighbour, then it is invalid
+                if (neighboursCount < 2) {
+                    invalidTiles.Add((x, y));
+                }
 
                 // Check four directions (up, down, left, right)
                 for (int i = 0; i < 4; i++) {
@@ -104,7 +127,7 @@ namespace MapEditor {
 
                     // Check if the new location is within the map
                     if (nx >= 0 && nx < 11 && ny >= 0 && ny < 11 && !visited[nx, ny]) {
-                        // Determine if a wall is in the way
+                        // Check if the new tile is reachable from this tile
                         bool isBlocked = false;
                         if (i == 0 && x > nx && horizontalWallStatus[nx, y]) // Wall above
                             isBlocked = true;
@@ -126,19 +149,19 @@ namespace MapEditor {
             }
 
             // Iterate over all the tiles
-            // and find the ones that are inaccessible or have a distance greater than 22
+            // and find the ones that are inaccessible, dead ends, or have a distance greater than 22
             for (int i = 0; i < 11; i++) {
                 for (int j = 0; j < 11; j++) {
                     if (!visited[i, j] || distance[i, j] > 22) {
-                        unreachableTiles.Add((i, j));
+                        invalidTiles.Add((i, j));
                     }
                 }
             }
 
-            if (unreachableTiles.Count > 0) {
+            if (invalidTiles.Count > 0) {
                 // If invalid tiles exist
                 // Display them & return false to stop the Save & Quit logic
-                DisplayInvalidTiles(unreachableTiles);
+                DisplayInvalidTiles(invalidTiles);
                 return false;
             }
 
@@ -192,94 +215,5 @@ namespace MapEditor {
 
             invalidTilesDisplaying = false;
         }
-
-        // /* TEST TEST TEST TEST TEST ONLY!!!!!!! */
-        //     // Initialise the wall state (all walls do not "have" walls)
-        //     bool[,] horizontalWalls = new bool[10, 11]; // HW: 10 rows, 11 walls per row
-        //     bool[,] verticalWalls = new bool[11, 10]; // VW: 11 rows, 10 walls per row
-        //     
-        //     // Set some walls as "having" walls
-        //     horizontalWalls[5, 5] = true; // Horizontal wall at (6, 6)
-        //     horizontalWalls[9, 1] = true;
-        //     horizontalWalls[9, 2] = true;
-        //     horizontalWalls[9, 3] = true;
-        //     horizontalWalls[9, 4] = true;
-        //     horizontalWalls[9, 5] = true;
-        //     horizontalWalls[9, 6] = true;
-        //     
-        //     verticalWalls[1, 0] = true;
-        //     verticalWalls[2, 0] = true;
-        //     verticalWalls[3, 0] = true;
-        //     verticalWalls[4, 0] = true;
-        //     verticalWalls[5, 0] = true;
-        //     verticalWalls[6, 0] = true;
-        //     verticalWalls[7, 0] = true;
-        //     verticalWalls[8, 0] = true;
-        //     verticalWalls[9, 0] = true;
-        //     verticalWalls[10, 0] = true;
-        //     
-        //     verticalWalls[0, 1] = true;
-        //     verticalWalls[1, 1] = true;
-        //     verticalWalls[2, 1] = true;
-        //     verticalWalls[3, 1] = true;
-        //     verticalWalls[4, 1] = true;
-        //     verticalWalls[5, 1] = true;
-        //     verticalWalls[6, 1] = true;
-        //     verticalWalls[7, 1] = true;
-        //     verticalWalls[8, 1] = true;
-        //     
-        //     verticalWalls[1, 2] = true;
-        //     verticalWalls[2, 2] = true;
-        //     verticalWalls[3, 2] = true;
-        //     verticalWalls[4, 2] = true;
-        //     verticalWalls[5, 2] = true;
-        //     verticalWalls[6, 2] = true;
-        //     verticalWalls[7, 2] = true;
-        //     verticalWalls[8, 2] = true;
-        //     verticalWalls[9, 2] = true;
-        //     
-        //     verticalWalls[0, 3] = true;
-        //     verticalWalls[1, 3] = true;
-        //     verticalWalls[2, 3] = true;
-        //     verticalWalls[3, 3] = true;
-        //     verticalWalls[4, 3] = true;
-        //     verticalWalls[5, 3] = true;
-        //     verticalWalls[6, 3] = true;
-        //     verticalWalls[7, 3] = true;
-        //     verticalWalls[8, 3] = true;
-        //     
-        //     verticalWalls[0, 4] = true;
-        //     verticalWalls[1, 4] = true;
-        //     verticalWalls[2, 4] = true;
-        //     verticalWalls[3, 4] = true;
-        //     verticalWalls[5, 4] = true;
-        //     verticalWalls[7, 4] = true;
-        //     verticalWalls[8, 4] = true;
-        //     verticalWalls[9, 4] = true;
-        //     
-        //     verticalWalls[1, 5] = true;
-        //     verticalWalls[2, 5] = true;
-        //     verticalWalls[3, 5] = true;
-        //     verticalWalls[4, 5] = true;
-        //     verticalWalls[5, 5] = true;
-        //     verticalWalls[6, 5] = true;
-        //     verticalWalls[7, 5] = true;
-        //     verticalWalls[8, 5] = true;
-        //     
-        //     verticalWalls[0, 6] = true;
-        //     verticalWalls[1, 6] = true;
-        //     verticalWalls[2, 6] = true;
-        //     verticalWalls[3, 6] = true;
-        //     verticalWalls[4, 6] = true;
-        //     verticalWalls[5, 6] = true;
-        //     verticalWalls[6, 6] = true;
-        //     verticalWalls[7, 6] = true;
-        //     verticalWalls[8, 6] = true;
-        //     verticalWalls[9, 6] = true;
-        //
-        //     WallData test = new WallData(horizontalWalls, verticalWalls);
-        //
-        //     // Perform check
-        //     CheckTileLegality(test);
     }
 }
