c412027c35c2f7667d3845fe174d1efb8dd45594
WaterCoFire <117800515+WaterCoFire@users.noreply.github.com>
2025-05-26 18:35:20 +0800

    Tutorial finish page code logic

---
+++ Assets/Scripts/Tutorial/TutorialFinishPage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Tutorial {
+    /**
+     * Manages the finish page in tutorial.
+     */
+    public class TutorialFinishPage : MonoBehaviour {
+        public Button homePageButton; // Return to home page button
+
+        // START FUNCTION
+        private void Start() {
+            // Set the action listener for the button
+            homePageButton.onClick.AddListener(OnHomePageButtonClick);
+        }
+
+        /* Action when the home page button is clicked */
+        private void OnHomePageButtonClick() {
+            // Back to the home page
+            PlayerPrefs.SetInt("MainPageAt", 0);
+            SceneManager.LoadScene("HomePage");
+
+            // Close this page
+            gameObject.SetActive(false);
+        }
+    }
+}
