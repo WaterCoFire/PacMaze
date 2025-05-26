88c68900700ee8c1b0b740b0539c1605f4ad0f07
WaterCoFire <117800515+WaterCoFire@users.noreply.github.com>
2025-05-26 11:40:41 +0800

    Tutorial controller code update

---
+++ Assets/Scripts/Tutorial/TutorialController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tutorial {
+    public class TutorialController : MonoBehaviour {
+        // All the dots
+        public List<GameObject> allDots;
+    }
+}
